2465621858c609299bca2369b1365e206d1045ec
Greg <antonioimportant@gmail.com>
2024-01-01 20:41:34 -0300

    user register logic + view + route done

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.temp') {{-- Aplicando um layout base na página --}}
+
+@section('title', 'Register') {{-- Setando o título da página --}}
+
+@section('content') {{-- Inserindo o conteúdo único dessa página na base do layout --}}
+    <h1>Register</h1>
+    <hr>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,3 +29,5 @@ Route::get('/logout', [LoginController::class, 'logout'])
 
 
 // User
+Route::get('/register', [UserController::class, 'create'])
+    ->name('user.create');
