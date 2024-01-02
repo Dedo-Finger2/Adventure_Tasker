ffc92a092eb7549cf615843d33304b92d735b065
Greg <antonioimportant@gmail.com>
2024-01-01 23:21:21 -0300

    difficulty and priority model added and view create added too

---
+++ resources/views/difficulty/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.temp') {{-- Aplicando um layout base na página --}}
+
+@section('title', 'Create Difficulty') {{-- Setando o título da página --}}
+
+@section('content') {{-- Inserindo o conteúdo único dessa página na base do layout --}}
+    <h1>Create Difficulty</h1>
+    <hr>
+    {{-- <livewire:user-create-form /> Formulário de cadastro em formato de componente livewire --}}
+@endsection
+
