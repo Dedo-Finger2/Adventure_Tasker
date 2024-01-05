6319c5b7a4d41735d3fb66f3ab9040b93f181567
Greg <antonioimportant@gmail.com>
2024-01-05 11:00:59 -0800

    fixed route to item store in stores page

--- resources/views/store/stores.blade.php
+++ resources/views/store/stores.blade.php
@@ -5,6 +5,6 @@
 @section('content') {{-- Inserindo o conteúdo único dessa página na base do layout --}}
     <h1>Stores</h1>
     <hr>
-    <a href="{{ route('') }}"></a>
+    <a href="{{ route('store.items') }}">Item store</a>
 @endsection
 
