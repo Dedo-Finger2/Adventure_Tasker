d87f9a7177e3a997589249455b9b87e8efa6b365
Greg <antonioimportant@gmail.com>
2024-01-05 16:35:52 -0800

    added flowbite styles

--- resources/views/layouts/temp.blade.php
+++ resources/views/layouts/temp.blade.php
@@ -5,10 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    {{-- Flowbite CSS --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    {{-- Flowbite css --}}
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 </head>
 <body>
-    @include('components.navbar')
-    @yield('content')
-    @include('components.footer')
+    @auth()
+        @include('components.navbar')
+        @yield('content')
+        @include('components.footer')
+    @endauth
+
+    @guest()
+        @yield('content')
+    @endguest
+
 </body>
 </html>
